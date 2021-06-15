831efb043a6750cee9973616c0a5ea7bebfb71dc
JoshEthan <joshuaeris@gmail.com>
2021-06-15 08:57:05 -0600

    Dashboard, Nav, Edit Profile

--- index.js
+++ index.js
@@ -1,56 +1,58 @@
-require('dotenv').config({ encoding: 'UTF-8' })
-require('dotenv').config({ path: __dirname + '/.env' });
-
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
+// const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
-//const routes = require('./routes');
-const PORT = process.env.PORT || 5000 
-//const User = require('./models/user');
-const cors = require('cors');
-const corsOptions = {
-    origin: "https://ta03-cse341.herokuapp.com/",
-    optionsSuccessStatus: 200
-  };
-const MONGODB_URI = process.env.MONGODB_URI;
+
+const PORT = process.env.PORT || 5000
+// const MONGODB_URI = process.env.MONGODB_URI;
+
 const app = express();
-const store = new MongoDBStore({
-  uri: MONGODB_URI, 
-  collection: 'sessions'
-});
+// const store = new MongoDBStore({
+//   uri: MONGODB_URI, 
+//   collection: 'sessions'
+// });
 //const csrfProtection  = csrf();
 
-const users = [];
+const cors = require('cors');
+// const corsOptions = {
+//     origin: "https://ta03-cse341.herokuapp.com/",
+//     optionsSuccessStatus: 200
+//   };
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-app
-.use(cors(corsOptions))
- // .use('/', routes);
 
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4
-};
+// const routes = require('./routes');
+const users = [];
+
+// app
+// .use(cors(corsOptions))
+// .use('/', routes);
+
+// const options = {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+//   family: 4
+// };
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use(session({
-  secret: 's3Cur3', 
-  resave: false, 
-  saveUninitialized: false,
-  store: store
- })
-);
-//app.use(csrfProtection);
+// app.use(
+//   session({
+//     secret: 'my secret',
+//     resave: false,
+//     saveUninitialized: false,
+//     store: store
+//   })
+// );
+// app.use(csrfProtection);
+// app.use(flash());
 
 // app.use((req, res, next) => {
 //   res.locals.isAuthenticated = req.session.isLoggedIn;
@@ -59,48 +61,44 @@ app.use(session({
 // });
 
 // app.use((req, res, next) => {
-//   //throw new Error('Sync Dummy');
-//   if (!req.session.user){
+//   if (!req.session.user) {
 //     return next();
 //   }
-//   User.findById(req.session.user._id)//('609583ea3f161a723a332044')//("60947956b893eb8bf3e04661")
-//     .then(user => { 
-//       //throw new Error('Dummy!);
-//       if (!user) {
+//   User.findById(req.session.user._id)
+//   .then(user => {
+//     if (!user) {
 //       return next();
 //     }
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => {
-//       //console.log(err));
-//       //next();
-//       //throw new Error(err);
-//       next(new Error(err));
-//     });
+//     req.user = user;
+//     next();
+//   })
+//   .catch(err => {
+//     next(new Error(err));
+//   });
 // });
 
 app.get('/', (req, res, next) => {
-    res.render('index', { pageTitle: 'Add User' });
+    res.render('editUserProfile', { pageTitle: 'Add User' });
   });
   
-  app.get('/users', (req, res, next) => {
-    res.render('users', {
-      pageTitle: 'User',
-      users: users,
-      hasUsers: users.length > 0
-    });
-  });
+  // app.get('/users', (req, res, next) => {
+  //   res.render('users', {
+  //     pageTitle: 'User',
+  //     // users: users,
+  //     // hasUsers: users.length > 0
+  //   });
+  // });
   
-  app.post('/add-user', (req, res, next) => {
-    users.push({ name: req.body.username });
-    res.redirect('/users');
-  });
-mongoose
-  .connect(MONGODB_URI, options)
-  .then(result => {
-    app.listen(PORT);
-})
-.catch(err => {
-  console.log(err);
-});
+  // app.post('/add-user', (req, res, next) => {
+  //   users.push({ name: req.body.username });
+  //   res.redirect('/users');
+  // });
+  app.listen(PORT)
+// mongoose
+//   .connect(MONGODB_URI, options)
+//   .then(result => {
+//     app.listen(PORT);
+// })
+// .catch(err => {
+//   console.log(err);
+// });
