325461110f11779a1ad34748fe785d566e0b95eb
stava-11 <77128788+stava-11@users.noreply.github.com>
2021-06-26 11:41:58 -0600

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -9,53 +9,67 @@ router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 router.post(
     '/login',
-    [
-        body('email')
-            .isEmail()
-            .withMessage('Please enter a valid email.')
-            .normalizeEmail(),
-        body('password', 'Password must be 4 alphanumeric characters')
-            .isLength({ min: 4 })
-            .isAlphanumeric()
-            .trim()
-    ],
+    // [
+    //     body('email')
+    //         .isEmail()
+    //         .withMessage('Please enter a valid email.')
+    //         .normalizeEmail(),
+    //     body('password', 'Password must be 4 alphanumeric characters')
+    //         .isLength({ min: 4 })
+    //         .isAlphanumeric()
+    //         .trim()
+    // ],
     authController.postLogin
 );
 router.post(
     '/signup',
-    [
-        check('email')
-            .isEmail()
-            .withMessage('Please enter a valid email.')
-            .custom((value, { req }) => {
-                // if (value === 'test@test.com') {
-                //     throw new Error('This email address is forbidden');
-                // }
-                // return true;
-                return User.findOne({ email: value })
-                    .then(userDoc => {
-                        if (userDoc) {
-                            return Promise.reject(
-                                'Email exists already. Please pick a different one.'
-                            );
-                        }
-                    });
-            })
-            .normalizeEmail(),
-        body('password',
-            'Please enter a password with only numbers and text and at least 5 characters.'
-        )
-            .isLength({ min: 5 })
-            .isAlphanumeric()
-            .trim(),
-        body('confirmPassword')
-        .trim()
+    [   check('name')
         .custom((value, { req }) => {
-            if (value !== req.body.password) {
-                throw new Error('Passwords have to match!');
-            }
-            return true;
-        })
+            // if (value === 'test@test.com') {
+            //     throw new Error('This email address is forbidden');
+            // }
+            // return true;
+            return User.findOne({ name: value })
+                .then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject(
+                            'Name exists already. Please pick a different one.'
+                        );
+                    }
+                });
+        }),
+        // check('email')
+        //     .isEmail()
+        //     .withMessage('Please enter a valid email.')
+        //     .custom((value, { req }) => {
+        //         // if (value === 'test@test.com') {
+        //         //     throw new Error('This email address is forbidden');
+        //         // }
+        //         // return true;
+        //         return User.findOne({ email: value })
+        //             .then(userDoc => {
+        //                 if (userDoc) {
+        //                     return Promise.reject(
+        //                         'Email exists already. Please pick a different one.'
+        //                     );
+        //                 }
+        //             });
+        //     })
+        //     .normalizeEmail(),
+        // body('password',
+        //     'Please enter a password with only numbers and text and at least 5 characters.'
+        // )
+        //     .isLength({ min: 5 })
+        //     .isAlphanumeric()
+        //     .trim(),
+        // body('confirmPassword')
+        // .trim()
+        // .custom((value, { req }) => {
+        //     if (value !== req.body.password) {
+        //         throw new Error('Passwords have to match!');
+        //     }
+        //     return true;
+        // })
     ],
     authController.postSignup);
 router.post('/logout', authController.postLogout);
--- routes/player.js
+++ routes/player.js
@@ -4,7 +4,9 @@ const { body } = require('express-validator');
 const playerController = require('../controllers/player');
 const isAuth = require('../middleware/is-auth');
 const router = express.Router();
-router.get('/editUserProfile', isAuth, playerController.getProfile);
-router.get('/dashboard', isAuth, playerController.getDashboard);
-router.use('/playGame', isAuth, playerController.getPlayGame);
+router.get('/editUserProfile', playerController.getProfile);
+router.get('/dashboard', playerController.getDashboard);
+router.use('/playGame', playerController.getPlayGame);
+router.post('/postGamePlay', playerController.postGamePlay);
+router.post('/postPlayerMove', playerController.postPlayerMove)
 module.exports = router;
